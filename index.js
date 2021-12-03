35a0e95f3249d5abd2a8f618dc640ea1efca37fb
Andersen <actfire@gmail.com>
2021-12-03 10:20:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,1 @@
+window.screenOrientation="sensor_landscape",loadLib("libs/min/laya.core.min.js"),loadLib("libs/min/laya.html.min.js"),loadLib("libs/min/laya.ui.min.js"),loadLib("libs/min/laya.physics.min.js"),loadLib("libs/fairygui/fairygui.js"),loadLib("libs/fairygui/rawinflate.min.js"),loadLib("js/bundle.js");
